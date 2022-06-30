ca10d23535c827c04f528abc972a6155f07ec8d1
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-06-30 15:54:11 +0800

    Modify Password Change page - Remove default backgound image, move the logo to the right column   above the create new password form and delete the quotes carousel   indicators. - Add password lock gif by linking it with img src.

--- application/views/access/password-change.php
+++ application/views/access/password-change.php
@@ -10,49 +10,21 @@
           <div class="col-lg-12">
             <div class="card overflow-hidden">
               <div class="row justify-content-center g-0">
-                <div class="col-lg-6">
-                  <div class="p-lg-5 p-4 auth-one-bg h-100">
-                    <div class="bg-overlay"></div>
-                    <div class="position-relative h-100 d-flex flex-column">
-                      <div class="mb-4">
-                        <a href="index.html" class="d-block">
-                          <img src="<?php echo base_url('public/assets') ?>/images/logo-light.png" alt="" height="50">
-                        </a>
-                      </div>
-                      <div class="mt-auto">
-                        <div class="mb-3">
-                          <i class="ri-double-quotes-l display-4 text-success"></i>
-                        </div>
-
-                        <div id="qoutescarouselIndicators" class="carousel slide" data-bs-ride="carousel">
-                          <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                          </div>
-                          <div class="carousel-inner text-center text-white-50 pb-5">
-                            <div class="carousel-item active">
-                              <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
-                            </div>
-                            <div class="carousel-item">
-                              <p class="fs-15 fst-italic">" The theme is really great with an amazing customer support."</p>
-                            </div>
-                            <div class="carousel-item">
-                              <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
-                            </div>
-                          </div>
-                        </div>
-                        <!-- end carousel -->
-                      </div>
-                    </div>
+                <div class="col-lg-6"> <img src="https://i.pinimg.com/originals/b9/82/77/b982770e4f0348a0a2f3b7ff25752c7b.gif" class="foodtrip-bg" alt="" height="50">
+                  <div class="position-relative h-100 d-flex flex-column">
                   </div>
                 </div>
                 <!-- end col -->
 
                 <div class="col-lg-6">
                   <div class="p-lg-5 p-4">
+                    <div class="mb-4">
+                      <a href="index.html" class="d-flex justify-content-center">
+                        <img src="<?php echo base_url('public/assets') ?>/images/logo-dark.png" alt="" height="50">
+                      </a>
+                    </div>
                     <h5 class="text-primary">Create new password</h5>
-                    <p class="text-muted">Your new password must be different from previous used password.</p>
+                    <p class="text-muted">Your new password must be different from previously used password.</p>
 
                     <div class="p-2">
                       <form action="auth-signin-basic.html">
