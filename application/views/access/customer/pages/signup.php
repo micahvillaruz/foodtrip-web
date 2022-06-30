0ed59a8d0ea766539a3bee34c358efd370078a3e
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-06-30 14:42:13 +0800

    Modify Customer Sign Up page - Remove default backgound image, move the logo to the right column   above the registration form and delete the quotes carousel indicators. - Add food sample pictures and use swiper with fade effect for   animation. - Add the fields needed to register a customer. - Add jQuery, Swiper Slider and Sign In with Google scripts and button.

--- application/views/access/customer/pages/signup.php
+++ application/views/access/customer/pages/signup.php
@@ -11,69 +11,81 @@
             <div class="card overflow-hidden m-0">
               <div class="row justify-content-center g-0">
                 <div class="col-lg-6">
-                  <div class="p-lg-5 p-4 auth-one-bg h-100">
-                    <div class="bg-overlay"></div>
-                    <div class="position-relative h-100 d-flex flex-column">
-                      <div class="mb-4">
-                        <a href="index.html" class="d-block">
-                          <img src="<?php echo base_url('public/assets') ?>/images/logo-light.png" alt="" height="50">
-                        </a>
+                  <div class="swiper effect-fade-swiper rounded h-100">
+                    <div class="swiper-wrapper">
+                      <div class="swiper-slide">
+                        <img src="<?php echo base_url('public/assets') ?>/images/small/food-1.jpg" alt="" class="foodtrip-bg" />
                       </div>
-                      <div class="mt-auto">
-                        <div class="mb-3">
-                          <i class="ri-double-quotes-l display-4 text-success"></i>
-                        </div>
-
-                        <div id="qoutescarouselIndicators" class="carousel slide" data-bs-ride="carousel">
-                          <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                          </div>
-                          <div class="carousel-inner text-center text-white pb-5">
-                            <div class="carousel-item active">
-                              <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
-                            </div>
-                            <div class="carousel-item">
-                              <p class="fs-15 fst-italic">" The theme is really great with an amazing customer support."</p>
-                            </div>
-                            <div class="carousel-item">
-                              <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
-                            </div>
-                          </div>
-                        </div>
-                        <!-- end carousel -->
-
+                      <div class="swiper-slide">
+                        <img src="<?php echo base_url('public/assets') ?>/images/small/food-2.jpg" alt="" class="foodtrip-bg" />
+                      </div>
+                      <div class="swiper-slide">
+                        <img src="<?php echo base_url('public/assets') ?>/images/small/food-3.jpg" alt="" class="foodtrip-bg" />
                       </div>
                     </div>
+                    <div class="swiper-pagination"></div>
                   </div>
                 </div>
 
                 <div class="col-lg-6">
                   <div class="p-lg-5 p-4">
                     <div>
+                      <div class="mb-4">
+                        <a href="index.html" class="d-flex justify-content-center">
+                          <img src="<?php echo base_url('public/assets') ?>/images/logo-dark.png" alt="" height="50">
+                        </a>
+                      </div>
                       <h5 class="text-primary">Register Account</h5>
-                      <p class="text-muted">Get your Free Velzon account now.</p>
+                      <p class="text-muted">Get your Free FoodTrip account now.</p>
                     </div>
 
                     <div class="mt-4">
                       <form class="needs-validation" novalidate action="index.html">
-
                         <div class="mb-3">
-                          <label for="useremail" class="form-label">Email <span class="text-danger">*</span></label>
-                          <input type="email" class="form-control" id="useremail" placeholder="Enter email address" required>
+                          <label for="first_name" class="form-label">First Name <span class="text-danger">*</span></label>
+                          <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Enter First Name" required>
                           <div class="invalid-feedback">
-                            Please enter email
+                            Please enter First Name
                           </div>
                         </div>
                         <div class="mb-3">
-                          <label for="username" class="form-label">Username <span class="text-danger">*</span></label>
-                          <input type="text" class="form-control" id="username" placeholder="Enter username" required>
+                          <label for="middle-name" class="form-label">Middle Name</label>
+                          <input type="text" class="form-control" id="middle_name" name="middle_name" placeholder="Enter Middle Name">
                           <div class="invalid-feedback">
-                            Please enter username
+                            Please enter Middle Name
                           </div>
                         </div>
-
+                        <div class="mb-3">
+                          <label for="last-name" class="form-label">Last Name <span class="text-danger">*</span></label>
+                          <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Enter Last Name">
+                          <div class="invalid-feedback">
+                            Please enter Last Name
+                          </div>
+                        </div>
+                        <div class="mb-3">
+                          <label for="useremail" class="form-label">Email Address <span class="text-danger">*</span></label>
+                          <input type="email" class="form-control" id="email_address" name="email_address" placeholder="Enter email address" required>
+                          <div class="invalid-feedback">
+                            Please enter Email Address
+                          </div>
+                        </div>
+                        <div class="mb-3">
+                          <label for="phone_number" class="form-label">Phone Number <span class="text-danger">*</span></label>
+                          <input type="number" class="form-control" id="phone_number" name="phone_number" placeholder="Enter Phone Number">
+                          <div class="invalid-feedback">
+                            Please enter Phone Number
+                          </div>
+                        </div>
+                        <div class="mb-3">
+                          <div class="col-lg-12">
+                            <label for="gender" class="form-label">Gender <span class="text-danger">*</span></label>
+                            <select class="js-example-basic-single" id="gender" name="gender">
+                              <option value="1">Male</option>
+                              <option value="2">Female</option>
+                              <option value="3">Others</option>
+                            </select>
+                          </div>
+                        </div>
                         <div class="mb-3">
                           <label class="form-label" for="password-input">Password</label>
                           <div class="position-relative auth-pass-inputgroup">
@@ -84,11 +96,6 @@
                             </div>
                           </div>
                         </div>
-
-                        <div class="mb-4">
-                          <p class="mb-0 fs-12 text-muted fst-italic">By registering you agree to the Velzon <a href="#" class="text-primary text-decoration-underline fst-normal fw-medium">Terms of Use</a></p>
-                        </div>
-
                         <div id="password-contain" class="p-3 bg-light mb-2 rounded">
                           <h5 class="fs-13">Password must contain:</h5>
                           <p id="pass-length" class="invalid fs-12 mb-2">Minimum <b>8 characters</b></p>
@@ -103,15 +110,10 @@
 
                         <div class="mt-4 text-center">
                           <div class="signin-other-title">
-                            <h5 class="fs-13 mb-4 title text-muted">Create account with</h5>
+                            <h5 class="fs-13 mb-4 title text-muted">OR</h5>
                           </div>
 
-                          <div>
-                            <button type="button" class="btn btn-primary btn-icon waves-effect waves-light"><i class="ri-facebook-fill fs-16"></i></button>
-                            <button type="button" class="btn btn-danger btn-icon waves-effect waves-light"><i class="ri-google-fill fs-16"></i></button>
-                            <button type="button" class="btn btn-dark btn-icon waves-effect waves-light"><i class="ri-github-fill fs-16"></i></button>
-                            <button type="button" class="btn btn-info btn-icon waves-effect waves-light"><i class="ri-twitter-fill fs-16"></i></button>
-                          </div>
+                          <div id="buttonDiv" class="d-flex justify-content-center"></div>
                         </div>
                       </form>
                     </div>
