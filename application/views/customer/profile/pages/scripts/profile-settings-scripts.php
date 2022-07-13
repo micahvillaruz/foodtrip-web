16a5deab0b6614db92d1da34dc9ef2f34230c7fc
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-07-13 23:55:31 +0800

    Modify Customer's Profile Settings Page

--- application/views/customer/profile/pages/scripts/profile-settings-scripts.php
+++ application/views/customer/profile/pages/scripts/profile-settings-scripts.php
@@ -1,5 +1,8 @@
     <!-- profile-setting init js -->
     <script src="<?php echo base_url('public/assets') ?>/js/pages/profile-setting.init.js"></script>
 
+    <!-- form wizard init -->
+    <script src="<?php echo base_url('public/assets') ?>/js/pages/form-wizard.init.js"></script>
+
     <!-- App js -->
     <script src="<?php echo base_url('public/assets') ?>/js/app.js"></script>
