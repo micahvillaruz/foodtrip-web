f287bb468598062f409e572fe088cea4b2b35c57
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-07-16 00:07:52 +0800

    Modify Customer's Product Details Page

--- application/views/customer/products/pages/product-details.php
+++ application/views/customer/products/pages/product-details.php
@@ -8,47 +8,11 @@
                             <div class="swiper product-thumbnail-slider p-2 rounded bg-light">
                                 <div class="swiper-wrapper">
                                     <div class="swiper-slide">
-                                        <img src="<?php echo base_url('public/assets') ?>/images/products/img-8.png" alt="" class="img-fluid d-block" />
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <img src="<?php echo base_url('public/assets') ?>/images/products/img-6.png" alt="" class="img-fluid d-block" />
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <img src="<?php echo base_url('public/assets') ?>/images/products/img-1.png" alt="" class="img-fluid d-block" />
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <img src="<?php echo base_url('public/assets') ?>/images/products/img-8.png" alt="" class="img-fluid d-block" />
+                                        <img src="<?php echo base_url('public/assets') ?>/images/food/jollibee/img-1.png" alt="" class="img-fluid d-block" />
                                     </div>
                                 </div>
-                                <div class="swiper-button-next bg-white shadow"></div>
-                                <div class="swiper-button-prev bg-white shadow"></div>
                             </div>
                             <!-- end swiper thumbnail slide -->
-                            <div class="swiper product-nav-slider mt-2">
-                                <div class="swiper-wrapper">
-                                    <div class="swiper-slide">
-                                        <div class="nav-slide-item">
-                                            <img src="<?php echo base_url('public/assets') ?>/images/products/img-8.png" alt="" class="img-fluid d-block" />
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="nav-slide-item">
-                                            <img src="<?php echo base_url('public/assets') ?>/images/products/img-6.png" alt="" class="img-fluid d-block" />
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="nav-slide-item">
-                                            <img src="<?php echo base_url('public/assets') ?>/images/products/img-1.png" alt="" class="img-fluid d-block" />
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="nav-slide-item">
-                                            <img src="<?php echo base_url('public/assets') ?>/images/products/img-8.png" alt="" class="img-fluid d-block" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- end swiper nav slide -->
                         </div>
                     </div>
                     <!-- end col -->
@@ -57,55 +21,126 @@
                         <div class="mt-xl-0 mt-5">
                             <div class="d-flex">
                                 <div class="flex-grow-1">
-                                    <h4>1 Pc Jollibee Chicken Joy</h4>
+                                    <h4>1 - pc. Chickenjoy w/ Half Jolly Spaghetti & Shanghai Super Meal</h4>
                                     <div class="hstack gap-3 flex-wrap">
-                    
-                                    </div>
-                                </div>
-                                <div class="flex-shrink-0">
-                                    <div>
-                                        <a href="apps-ecommerce-add-product.html" class="btn btn-light" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit"><i class="ri-pencil-fill align-bottom"></i></a>
+                                        <div><a href="#" class="text-primary d-block">Super Meals</a></div>
+                                        <div class="vr"></div>
+                                        <div class="text-muted">Restaurant : <span class="text-body fw-medium">Jollibee</span></div>
+                                        <div class="vr"></div>
+                                        <div class="text-muted">Published : <span class="text-body fw-medium">12 Oct, 2021</span></div>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="row mt-4">
                                 <div class="col-lg-3 col-sm-6">
-                                    <div class="p-2 border border-dashed rounded">
+                                    <div class="p-2 rounded">
                                         <div class="d-flex align-items-center">
-                                            <div class="avatar-sm me-2">
-                                                <div class="avatar-title rounded bg-transparent text-success fs-24">
-                                                    <i class="ri-money-dollar-circle-fill"></i>
-                                                </div>
-                                            </div>
-                                            <div class="flex-grow-1">
-                                                <p class="text-muted mb-1">Price :</p>
-                                                <h5 class="mb-0">$120.40</h5>
-                                            </div> 
-                                            <div class="col-lg-12">
-                                            <button type="button" class="btn btn-primary waves-effect waves-light"><i class="ri-shopping-cart-2-line"></i> Add to Cart</button>
-                                            </div>
-                                            <div class="col-lg-12 text-end">
-                                            <button type="button" class="btn btn-success waves-effect waves-light"><i class="ri-bank-card-line"></i> Order Now</button>
-                                            </div>
+                                            <h2 class="mb-0 text-secondary">₱185.00</h2>
                                         </div>
                                     </div>
                                 </div>
-
+                                <!-- end col -->
                             </div>
-                            </div>
 
-                            <div class="mt-4 text-muted">
+                            <div class="mt-4">
                                 <h5 class="fs-14">Description :</h5>
-                                <p>1 pc chicken joy, Drink, Gravy, Rice
-                                </p>
+                                <p>Your Jollibee favorites in one Supermeal: 1pc Chickenjoy with Shanghai and Half Jolly Spaghetti, with rice and drink.</p>
                             </div>
-                                                <!-- end row -->
-                                    <!-- end col -->
+
+                            <div class="product-content mt-5">
+                                <h5 class="fs-14 mb-3">Dish Description :</h5>
+                                <nav>
+                                    <ul class="nav nav-tabs nav-tabs-custom nav-success" id="nav-tab" role="tablist">
+                                        <li class="nav-item">
+                                            <a class="nav-link active" id="nav-speci-tab" data-bs-toggle="tab" href="#nav-speci" role="tab" aria-controls="nav-speci" aria-selected="true">Specification</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" id="nav-detail-tab" data-bs-toggle="tab" href="#nav-detail" role="tab" aria-controls="nav-detail" aria-selected="false">Details</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                                <div class="tab-content border border-top-0 p-4" id="nav-tabContent">
+                                    <div class="tab-pane fade show active" id="nav-speci" role="tabpanel" aria-labelledby="nav-speci-tab">
+                                        <div class="table-responsive">
+                                            <table class="table mb-0">
+                                                <tbody>
+                                                    <tr>
+                                                        <th scope="row" style="width: 200px;">Category</th>
+                                                        <td>Super Meals</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th scope="row">Restaurant</th>
+                                                        <td>Jollibee</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane fade" id="nav-detail" role="tabpanel" aria-labelledby="nav-detail-tab">
+                                        <div>
+                                            <h5 class="font-size-16 mb-3">
+                                                1 - pc. Chickenjoy w/ Half Jolly Spaghetti & Shanghai Super Meal</h5>
+                                            <p>Your Jollibee favorites in one Supermeal: 1pc Chickenjoy with Shanghai and Half Jolly Spaghetti, with rice and drink.</p>
+                                        </div>
+                                    </div>
                                 </div>
-                                <!-- end Ratings & Reviews -->
                             </div>
-                            <!-- end card body -->
+
+                            <div class="d-flex gap-4 mt-4">
+                                <div>
+                                    <button type="button" class="btn btn-primary waves-effect waves-light"><i class="ri-shopping-cart-2-line"></i> Add to Cart</button>
+                                </div>
+                                <div>
+                                    <button type="button" class="btn btn-success waves-effect waves-light"><i class="ri-bank-card-line"></i> Order Now</button>
+                                </div>
+                            </div>
+                            <!-- product-content -->
+                        </div>
+                    </div>
+                    <!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end card body -->
+        </div>
+        <!-- end card -->
+        <div class="card">
+            <div class="card-body">
+                <div class="row gx-lg-5">
+                    <div class="col-3 inline-block border-end">
+                        <!-- Rounded-circle Image -->
+                        <div class="row">
+                            <div class="col-4">
+                                <img class="rounded-circle avatar-lg shadow border m-3" src="<?php echo base_url('public/assets') ?>/images/restaurants/jollibee.jpg">
+                            </div>
+                            <div class="col-8">
+                                <div class="mt-4">
+                                    <h3 class="fs-15 mb-0">Jollibee</h3>
+                                    <span class="text-muted mt-0 mb-2 d-block">Fast Food</span>
+                                    <button type="button" class="btn btn-sm btn-soft-primary waves-effect waves-light"><i class="ri-store-2-line"></i> View Restaurant</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-9">
+                        <div>
+                            <div class="table-responsive">
+                                <table class="table mt-3 table-borderless">
+                                    <tbody>
+                                        <tr>
+                                            <td><span class="fw-medium me-2">Opening Hours : </span> 9:00 AM - 8:00 PM</td>
+                                            <td><span class="fw-medium me-2">Phone No. : </span> +63 917 131 8000</td>
+                                            <td><span class="fw-medium me-2">Landline No. : </span> (02) 8-7000</td>
+                                        </tr>
+                                        <tr>
+                                            <td><span class="fw-medium me-2">Website : </span> <a href="http://www.jollibee.com.ph/" class="link-primary">www.jollibee.com.ph</a></td>
+                                            <td><span class="fw-medium me-2">Email : </span> president@jollibee.com.ph</td>
+                                            <td><span class="fw-medium me-2">Joined : </span> 20, June 2021</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                     <!-- end col -->
--- application/views/customer/products/pages/scripts/product-details-scripts.php
+++ application/views/customer/products/pages/scripts/product-details-scripts.php
@@ -4,5 +4,8 @@
 <!-- ecommerce product details init -->
 <script src="<?php echo base_url('public/assets') ?>/js/pages/ecommerce-product-details.init.js"></script>
 
+<!-- input spin init -->
+<script src="<?php echo base_url('public/assets') ?>/js/pages/form-input-spin.init.js"></script>
+
 <!-- App js -->
 <script src="<?php echo base_url('public/assets') ?>/js/app.js"></script>
