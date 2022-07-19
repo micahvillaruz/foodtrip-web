b5f205e607f57171176c4d5961ed69750f8d2efd
emmanperalta <emmanperalta4@gmail.com>
2022-07-19 22:19:57 +0800

    Restoadmin-routes -order details -invoice-details

--- application/views/restoadmin/invoices-management/pages/invoice-details.php
+++ application/views/restoadmin/invoices-management/pages/invoice-details.php
@@ -10,15 +10,16 @@
                 <img src="<?php echo base_url('public/assets') ?>/images/logo-light.png" class="card-logo card-logo-light" alt="logo light" height="55">
                 <div class="mt-sm-5 mt-4">
                   <h6 class="text-muted text-uppercase fw-semibold">Address</h6>
-                  <p class="text-muted mb-1" id="address-details">California, United States</p>
-                  <p class="text-muted mb-0" id="zip-code"><span>Zip-code:</span> 90201</p>
+                  <p class="text-muted mb-1" id="restaurant-address">10/F Jollibee Plaza Building Emerald Ave.Quezon, North Luzon</p>
+                  <p class="text-muted mb-0" id="zip-code"><span>Zip-code:</span> 1008</p>
                 </div>
               </div>
               <div class="flex-shrink-0 mt-sm-0 mt-3">
-                <h6><span class="text-muted fw-normal">Legal Registration No:</span><span id="legal-register-no">987654</span></h6>
-                <h6><span class="text-muted fw-normal">Email:</span><span id="email">velzon@themesbrand.com</span></h6>
-                <h6><span class="text-muted fw-normal">Website:</span> <a href="https://themesbrand.com/" class="link-primary" target="_blank" id="website">www.themesbrand.com</a></h6>
-                <h6 class="mb-0"><span class="text-muted fw-normal">Contact No: </span><span id="contact-no"> +(01) 234 6789</span></h6>
+                <h6><span class="text-muted fw-normal">Restaurant Name:</span><span id="restaurant-name"> Jollibee</span></h6>
+                <h6><span class="text-muted fw-normal">Category:</span><span id="restaurant-category"> Fast Food</span></h6>
+                <h6><span class="text-muted fw-normal">Email:</span><span id="restaurant-email"> feedback@jollibee.com.ph</span></h6>
+                <h6><span class="text-muted fw-normal">Website:</span><span id="restaurant-website"> www.jollibee.com.ph</span></h6>
+                <h6><span class="text-muted fw-normal">Landline No:</span><span id="restaurant-landline"> (02) 8-7000</span></h6>
               </div>
             </div>
           </div>
@@ -29,23 +30,23 @@
           <div class="card-body p-4">
             <div class="row g-3">
               <div class="col-lg-3 col-6">
-                <p class="text-muted mb-2 text-uppercase fw-semibold">Invoice No</p>
-                <h5 class="fs-14 mb-0">#VL<span id="invoice-no">25000355</span></h5>
+                <p class="text-muted mb-2 text-uppercase fw-semibold">Payment No:</p>
+                <h5 class="fs-14 mb-0">#<span id="invoice-no">1400090321</span></h5>
               </div>
               <!--end col-->
               <div class="col-lg-3 col-6">
                 <p class="text-muted mb-2 text-uppercase fw-semibold">Date</p>
-                <h5 class="fs-14 mb-0"><span id="invoice-date">23 Nov, 2021</span> <small class="text-muted" id="invoice-time">02:36PM</small></h5>
+                <h5 class="fs-14 mb-0"><span id="invoice-date">09 Jul, 2022 2:05 PM</span> <small class="text-muted" id="invoice-time">02:36PM</small></h5>
               </div>
               <!--end col-->
               <div class="col-lg-3 col-6">
                 <p class="text-muted mb-2 text-uppercase fw-semibold">Payment Status</p>
-                <span class="badge badge-soft-success fs-11" id="payment-status">Paid</span>
+                <span class="badge badge-soft-success fs-11" id="payment-status">PAID</span>
               </div>
               <!--end col-->
               <div class="col-lg-3 col-6">
-                <p class="text-muted mb-2 text-uppercase fw-semibold">Total Amount</p>
-                <h5 class="fs-14 mb-0">$<span id="total-amount">755.96</span></h5>
+                <p class="text-muted mb-2 text-uppercase fw-semibold">Payment Total</p>
+                <h5 class="fs-14 mb-0">₱<span id="total-amount">840.00</span></h5>
               </div>
               <!--end col-->
             </div>
@@ -58,20 +59,12 @@
           <div class="card-body p-4 border-top border-top-dashed">
             <div class="row g-3">
               <div class="col-6">
-                <h6 class="text-muted text-uppercase fw-semibold mb-3">Billing Address</h6>
-                <p class="fw-medium mb-2" id="billing-name">David Nichols</p>
-                <p class="text-muted mb-1" id="billing-address-line-1">305 S San Gabriel Blvd</p>
-                <p class="text-muted mb-1"><span>Phone: +</span><span id="billing-phone-no">(123) 456-7890</span></p>
-                <p class="text-muted mb-0"><span>Tax: </span><span id="billing-tax-no">12-3456789</span> </p>
-              </div>
-              <!--end col-->
-              <div class="col-6">
                 <h6 class="text-muted text-uppercase fw-semibold mb-3">Shipping Address</h6>
-                <p class="fw-medium mb-2" id="shipping-name">David Nichols</p>
-                <p class="text-muted mb-1" id="shipping-address-line-1">305 S San Gabriel Blvd</p>
-                <p class="text-muted mb-1"><span>Phone: +</span><span id="shipping-phone-no">(123) 456-7890</span></p>
+                <p class="fw-medium mb-2" id="shipping-name">Joseph Parker</p>
+                <p class="text-muted mb-1" id="shipping-address-line-1">2186 Joyce Street Rocky Mount, Quezon, North Luzon</p>
+                <p class="text-muted mb-1"><span>Phone: +</span><span id="shipping-phone-no">639264771621</span></p>
+                <h6><span class="text-muted fw-normal">Email:</span><span id="email">josephparker@gmail.com</span></h6>
               </div>
-              <!--end col-->
             </div>
             <!--end row-->
           </div>
@@ -85,10 +78,10 @@
                 <thead>
                   <tr class="table-active">
                     <th scope="col" style="width: 50px;">#</th>
-                    <th scope="col">Product Details</th>
-                    <th scope="col">Rate</th>
+                    <th scope="col">Dish Name</th>
+                    <th scope="col">Dish Price</th>
                     <th scope="col">Quantity</th>
-                    <th scope="col" class="text-end">Amount</th>
+                    <th scope="col" class="text-end">Sub Total</th>
                   </tr>
                 </thead>
                 <tbody id="products-list">
@@ -98,9 +91,9 @@
                       <span class="fw-medium">Sweatshirt for Men (Pink)</span>
                       <p class="text-muted mb-0">Graphic Print Men & Women Sweatshirt</p>
                     </td>
-                    <td>$119.99</td>
+                    <td>₱250.00</td>
                     <td>02</td>
-                    <td class="text-end">$239.98</td>
+                    <td class="text-end">₱500.00</td>
                   </tr>
                   <tr>
                     <th scope="row">02</th>
@@ -108,9 +101,9 @@
                       <span class="fw-medium">Noise NoiseFit Endure Smart Watch</span>
                       <p class="text-muted mb-0">32.5mm (1.28 Inch) TFT Color Touch Display</p>
                     </td>
-                    <td>$94.99</td>
+                    <td>₱200.00</td>
                     <td>01</td>
-                    <td class="text-end">$94.99</td>
+                    <td class="text-end">₱200.00</td>
                   </tr>
                   <tr>
                     <th scope="row">03</th>
@@ -118,19 +111,9 @@
                       <span class="fw-medium">350 ml Glass Grocery Container</span>
                       <p class="text-muted mb-0">Glass Grocery Container (Pack of 3, White)</p>
                     </td>
-                    <td>$24.99</td>
+                    <td>₱140.00</td>
                     <td>01</td>
-                    <td class="text-end">$24.99</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">04</th>
-                    <td class="text-start">
-                      <span class="fw-medium">Fabric Dual Tone Living Room Chair</span>
-                      <p class="text-muted mb-0">Chair (White)</p>
-                    </td>
-                    <td>$340.00</td>
-                    <td>01</td>
-                    <td class="text-end">$340.00</td>
+                    <td class="text-end">₱140.00</td>
                   </tr>
                 </tbody>
               </table>
@@ -139,25 +122,9 @@
             <div class="border-top border-top-dashed mt-2">
               <table class="table table-borderless table-nowrap align-middle mb-0 ms-auto" style="width:250px">
                 <tbody>
-                  <tr>
-                    <td>Sub Total</td>
-                    <td class="text-end">$699.96</td>
-                  </tr>
-                  <tr>
-                    <td>Estimated Tax (12.5%)</td>
-                    <td class="text-end">$44.99</td>
-                  </tr>
-                  <tr>
-                    <td>Discount <small class="text-muted">(VELZON15)</small></td>
-                    <td class="text-end">- $53.99</td>
-                  </tr>
-                  <tr>
-                    <td>Shipping Charge</td>
-                    <td class="text-end">$65.00</td>
-                  </tr>
                   <tr class="border-top border-top-dashed fs-15">
-                    <th scope="row">Total Amount</th>
-                    <th class="text-end">$755.96</th>
+                    <th scope="row">Total Amount:</th>
+                    <th class="text-end">₱840.00</th>
                   </tr>
                 </tbody>
               </table>
@@ -165,21 +132,10 @@
             </div>
             <div class="mt-3">
               <h6 class="text-muted text-uppercase fw-semibold mb-3">Payment Details:</h6>
-              <p class="text-muted mb-1">Payment Method: <span class="fw-medium" id="payment-method">Mastercard</span></p>
-              <p class="text-muted mb-1">Card Holder: <span class="fw-medium" id="card-holder-name">David Nichols</span></p>
-              <p class="text-muted mb-1">Card Number: <span class="fw-medium" id="card-number">xxx xxxx xxxx 1234</span></p>
-              <p class="text-muted">Total Amount: <span class="fw-medium" id="">$ </span><span id="card-total-amount">755.96</span></p>
-            </div>
-            <div class="mt-4">
-              <div class="alert alert-info">
-                <p class="mb-0"><span class="fw-semibold">NOTES:</span>
-                  <span id="note">All accounts are to be paid within 7 days from receipt of invoice. To be paid by cheque or
-                    credit card or direct payment online. If account is not paid within 7
-                    days the credits details supplied as confirmation of work undertaken
-                    will be charged the agreed quoted fee noted above.
-                  </span>
-                </p>
-              </div>
+              <p class="text-muted mb-1">Payment Number: <span class="fw-medium" id="payment-number">1400090321</span></p>
+              <p class="text-muted mb-1">Payment Status: <span class="fw-medium" id="payment-status">In Process</span></p>
+              <p class="text-muted mb-1">Date Paid: <span class="fw-medium" id="date-paid">09 Jul, 2022 2:05 PM 02:36PM</span></p>
+              <p class="text-muted">Total Amount: <span class="fw-medium" id="">₱ </span><span id="card-total-amount">₱840.00</span></p>
             </div>
             <div class="hstack gap-2 justify-content-end d-print-none mt-4">
               <a href="javascript:window.print()" class="btn btn-success"><i class="ri-printer-line align-bottom me-1"></i> Print</a>
