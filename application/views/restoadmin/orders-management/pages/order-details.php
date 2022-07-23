3dcb54266e887d65a697c1f8c18954f14729c597
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-07-23 11:52:23 +0800

    Modify RestoAdmin Order Details Page
    Co-authored-by: James Paul L. Tulod <paultulod@pm.me>

--- application/views/restoadmin/orders-management/pages/order-details.php
+++ application/views/restoadmin/orders-management/pages/order-details.php
@@ -3,7 +3,7 @@
     <div class="card">
       <div class="card-header">
         <div class="d-flex align-items-center">
-          <h5 class="card-title flex-grow-1 mb-0">Order #1400090321</h5>
+          <h5 class="card-title flex-grow-1 mb-0">Order #97464507680</h5>
           <div class="flex-shrink-0">
             <a href="apps-invoices-details.html" class="btn btn-success btn-sm"><i class="ri-download-2-fill align-middle me-1"></i> Invoice</a>
           </div>
@@ -14,10 +14,10 @@
           <table class="table table-nowrap align-middle table-borderless mb-0">
             <thead class="table-light text-muted">
               <tr>
-                <th scope="col">Product Details</th>
-                <th scope="col">Item Price</th>
+                <th scope="col">Dish Name</th>
+                <th scope="col">Dish Price</th>
                 <th scope="col">Quantity</th>
-                <th scope="col" class="text-end">Total Amount</th>
+                <th scope="col" class="text-end">Sub Total</th>
               </tr>
             </thead>
             <tbody>
@@ -25,67 +25,64 @@
                 <td>
                   <div class="d-flex">
                     <div class="flex-shrink-0 avatar-md bg-light rounded p-1">
-                      <img src="<?php echo base_url('public/assets') ?>/images/products/img-8.png" alt="" class="img-fluid d-block">
+                      <img src="<?php echo base_url('public/assets') ?>/images/food/jollibee/img-27.png" alt="" class="img-fluid d-block">
                     </div>
                     <div class="flex-grow-1 ms-3">
-                      <h5 class="fs-15"><a href="apps-ecommerce-product-details.html" class="link-primary">Sweatshirt for Men (Pink)</a></h5>
-
+                      <h5 class="fs-15"><a href="#!" class="link-primary">8pc Chickenjoy w/ Jolly Spaghetti Pan</a></h5>
+                      <p class="text-muted mb-0">Category: <span class="fw-medium">Family Meals</span></p>
                     </div>
                   </div>
                 </td>
-                <td>$119.99</td>
-                <td>02</td>
-
+                <td>₱776.00</td>
+                <td>1</td>
                 <td class="fw-medium text-end">
-                  $239.98
+                  ₱776.00
                 </td>
               </tr>
               <tr>
                 <td>
                   <div class="d-flex">
                     <div class="flex-shrink-0 avatar-md bg-light rounded p-1">
-                      <img src="<?php echo base_url('public/assets') ?>/images/products/img-7.png" alt="" class="img-fluid d-block">
+                      <img src="<?php echo base_url('public/assets') ?>/images/food/jollibee/img-15.png" alt="" class="img-fluid d-block">
                     </div>
                     <div class="flex-grow-1 ms-3">
-                      <h5 class="fs-15"><a href="apps-ecommerce-product-details.html" class="link-primary">Noise NoiseFit Endure Smart Watch</a></h5>
-
+                      <h5 class="fs-15"><a href="#!" class="link-primary">Amazing Aloha Champ Jr.</a></h5>
+                      <p class="text-muted mb-0">Category: <span class="fw-medium">Burgers</span></p>
                     </div>
                   </div>
                 </td>
-                <td>$94.99</td>
-                <td>01</td>
-
+                <td>₱109.00</td>
+                <td>5</td>
                 <td class="fw-medium text-end">
-                  $94.99
+                  ₱545.00
                 </td>
               </tr>
               <tr>
                 <td>
                   <div class="d-flex">
                     <div class="flex-shrink-0 avatar-md bg-light rounded p-1">
-                      <img src="<?php echo base_url('public/assets') ?>/images/products/img-3.png" alt="" class="img-fluid d-block">
+                      <img src="<?php echo base_url('public/assets') ?>/images/food/jollibee/img-28.png" alt="" class="img-fluid d-block">
                     </div>
                     <div class="flex-grow-1 ms-3">
-                      <h5 class="fs-15"><a href="apps-ecommerce-product-details.html" class="link-primary">350 ml Glass Grocery Container</a></h5>
-
+                      <h5 class="fs-15"><a href="#!" class="link-primary">1pc Burger Steak w/ Shanghai w/ Drink</a></h5>
+                      <p class="text-muted mb-0">Category: <span class="fw-medium">Burger Steak</span></p>
                     </div>
                   </div>
                 </td>
-                <td>$24.99</td>
-                <td>01</td>
-
+                <td>₱143.00</td>
+                <td>3</td>
                 <td class="fw-medium text-end">
-                  $24.99
+                  ₱429.00
                 </td>
               </tr>
               <tr class="border-top border-top-dashed">
-                <td colspan="3"></td>
+                <td colspan="2"></td>
                 <td colspan="2" class="fw-medium p-0">
                   <table class="table table-borderless mb-0">
                     <tbody>
                       <tr class="border-top border-top-dashed">
-                        <th scope="row">Total Amount :</th>
-                        <th class="text-end">$415.96</th>
+                        <th scope="row">Total (PHP) :</th>
+                        <th class="text-end">₱1,750.00</th>
                       </tr>
                     </tbody>
                   </table>
@@ -102,8 +99,7 @@
         <div class="d-sm-flex align-items-center">
           <h5 class="card-title flex-grow-1 mb-0">Order Status</h5>
           <div class="flex-shrink-0 mt-2 mt-sm-0">
-            <a href="javasccript:void(0;)" class="btn btn-soft-info btn-sm mt-2 mt-sm-0 shadow-none"><i class="ri-map-pin-line align-middle me-1"></i> Change Address</a>
-            <a href="javasccript:void(0;)" class="btn btn-soft-danger btn-sm mt-2 mt-sm-0 shadow-none"><i class="mdi mdi-archive-remove-outline align-middle me-1"></i> Cancel Order</a>
+            <a href="javasccript:void(0;)" class="btn btn-soft-info btn-sm mt-2 mt-sm-0 shadow-none"><i class="ri-quill-pen-line align-middle me-1"></i> Update Status</a>
           </div>
         </div>
       </div>
@@ -116,11 +112,11 @@
                   <div class="d-flex align-items-center">
                     <div class="flex-shrink-0 avatar-xs">
                       <div class="avatar-title bg-success rounded-circle shadow">
-                        <i class="ri-shopping-bag-line"></i>
+                        <i class="mdi mdi-food-takeout-box-outline"></i>
                       </div>
                     </div>
                     <div class="flex-grow-1 ms-3">
-                      <h6 class="fs-15 mb-0 fw-semibold">In Process - <span class="fw-normal">Sat, 09 Jul 2022</span></h6>
+                      <h6 class="fs-15 mb-0 fw-semibold">In Process - <span class="fw-normal">Mon, 18 Jul 2022</span></h6>
                     </div>
                   </div>
                 </a>
@@ -128,15 +124,13 @@
               <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                 <div class="accordion-body ms-2 ps-5 pt-0">
                   <h6 class="mb-1">An order has been placed.</h6>
-                  <p class="text-muted">Wed, 15 Dec 2021 - 02:05PM</p>
-
+                  <p class="text-muted">Mon, 18 Jul 2022 - 11:30AM</p>
                 </div>
               </div>
             </div>
-
             <div class="accordion-item border-0">
-              <div class="accordion-header" id="headingThree">
-                <a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              <div class="accordion-header" id="headingTwo">
+                <a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                   <div class="d-flex align-items-center">
                     <div class="flex-shrink-0 avatar-xs">
                       <div class="avatar-title bg-success rounded-circle shadow">
@@ -144,23 +138,22 @@
                       </div>
                     </div>
                     <div class="flex-grow-1 ms-3">
-                      <h6 class="fs-15 mb-1 fw-semibold">On the Way- <span class="fw-normal">Sat, 09 Jul 2022</span></h6>
+                      <h6 class="fs-15 mb-1 fw-semibold">On the Way - <span class="fw-normal">Mon, 18 Jul 2022</span></h6>
                     </div>
                   </div>
                 </a>
               </div>
-              <div id="collapseThree" class="accordion-collapse collapse show" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+              <div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                 <div class="accordion-body ms-2 ps-5 pt-0">
-                  <h6 class="fs-14">Grab Express- MFDS1400457854</h6>
-                  <h6 class="mb-1">Your item has been picked up by the courier partner.</h6>
-                  <p class="text-muted mb-0">Sat, 09 Jul 2022 - 02:48 PM</p>
+                  <h6 class="fs-14">LalaMove - 26325016208</h6>
+                  <h6 class="mb-1">The order has been picked up by the courier partner.</h6>
+                  <p class="text-muted mb-0">Mon, 18 Jul 2021 - 12:15PM</p>
                 </div>
               </div>
             </div>
-
             <div class="accordion-item border-0">
-              <div class="accordion-header" id="headingFive">
-                <a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseFile" aria-expanded="false">
+              <div class="accordion-header" id="headingThree">
+                <a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                   <div class="d-flex align-items-center">
                     <div class="flex-shrink-0 avatar-xs">
                       <div class="avatar-title bg-success rounded-circle shadow">
@@ -168,12 +161,16 @@
                       </div>
                     </div>
                     <div class="flex-grow-1 ms-3">
-                      <h6 class="fs-14 mb-0 fw-semibold">Delivered</h6>
-                      <h6 class="mb-1">The order has been delivered.</h6>
-                      <p class="text-muted mb-0">Sat, 09 Jul 2022 - 03:29 PM</p>
+                      <h6 class="fs-15 mb-1 fw-semibold">Delivered - <span class="fw-normal">Mon, 18 Jul 2022</span></h6>
                     </div>
                   </div>
                 </a>
+              </div>
+              <div id="collapseThree" class="accordion-collapse collapse show" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body ms-2 ps-5 pt-0">
+                  <h6 class="mb-1">The order has been delivered.</h6>
+                  <p class="text-muted mb-0">Mon, 18 Jul 2021 - 12:29PM</p>
+                </div>
               </div>
             </div>
           </div>
@@ -197,22 +194,21 @@
       <div class="card-body">
         <div class="text-center">
           <lord-icon src="https://cdn.lordicon.com/uetqnvvg.json" trigger="loop" colors="primary:#4b38b3,secondary:#0ab39c" style="width:80px;height:80px"></lord-icon>
-          <h5 class="fs-16 mt-2">Grab Express</h5>
-          <p class="text-muted mb-0">Tracking Number: MFDS1400457854</p>
-          <p class="text-muted mb-0">Driving Name : Benedict Juarez</p>
-          <p class="text-muted mb-0">Driving Phone : 09274538396</p>
+          <h5 class="fs-16 mt-2">LalaMove</h5>
+          <p class="text-muted mb-0">Tracking No.: 26325016208</p>
+          <p class="text-muted mb-0">Driver Name : Adonis Saide Figueroa</p>
+          <p class="text-muted mb-0">Driver Phone : 09918291232</p>
         </div>
       </div>
     </div>
     <!--end card-->
-
 
     <div class="card">
       <div class="card-header">
         <div class="d-flex">
           <h5 class="card-title flex-grow-1 mb-0">Customer Details</h5>
           <div class="flex-shrink-0">
-            <a href="javascript:void(0);" class="link-secondary">View Profile</a>
+            <a href="javascript:void(0);" class="link-secondary">View Customer</a>
           </div>
         </div>
       </div>
@@ -220,29 +216,29 @@
         <ul class="list-unstyled mb-0 vstack gap-3">
           <li>
             <div class="d-flex align-items-center">
-              <div class="flex-grow-1 ms-1">
-                <h6 class="fs-14 mb-1">Joseph Parkers</h6>
+              <div class="flex-grow-1 ms-3">
+                <h6 class="fs-14 mb-1">Avrielle Haven Fernandez Juarez</h6>
                 <p class="text-muted mb-0">Customer</p>
               </div>
             </div>
           </li>
-          <li><i class="ri-mail-line me-2 align-middle text-muted fs-16"></i>josephparker@gmail.com</li>
-          <li><i class="ri-phone-line me-2 align-middle text-muted fs-16"></i>09264771621</li>
+          <li><i class="ri-mail-line me-2 align-middle text-muted fs-16"></i>avijuarez@gmail.com</li>
+          <li><i class="ri-phone-line me-2 align-middle text-muted fs-16"></i>09872537193</li>
         </ul>
       </div>
     </div>
-
+    <!--end card-->
     <div class="card">
       <div class="card-header">
         <h5 class="card-title mb-0"><i class="ri-map-pin-line align-middle me-1 text-muted"></i> Shipping Address</h5>
       </div>
       <div class="card-body">
         <ul class="list-unstyled vstack gap-2 fs-13 mb-0">
-          <li class="fw-medium fs-14">Joseph Parker</li>
-          <li>09264771621</li>
-          <li>2186 Joyce Street Rocky Mount</li>
-          <li>Quezon </li>
-          <li>North Luzon,</li>
+          <li class="fw-medium fs-14">Avrielle Haven Fernandez Juarez</li>
+          <li>09872537193</li>
+          <li>1071 Del Monte Avenue</li>
+          <li>Barangay Paltok, Quezon City</li>
+          <li>Metro Manila, 1105</li>
         </ul>
       </div>
     </div>
@@ -255,34 +251,27 @@
       <div class="card-body">
         <div class="d-flex align-items-center mb-2">
           <div class="flex-shrink-0">
-            <p class="text-muted mb-0">Transactions:</p>
+            <p class="text-muted mb-0">Payment No:</p>
           </div>
           <div class="flex-grow-1 ms-2">
-            <h6 class="mb-0">#VLZ124561278124</h6>
+            <h6 class="mb-0">60524012573</h6>
           </div>
         </div>
         <div class="d-flex align-items-center mb-2">
           <div class="flex-shrink-0">
-            <p class="text-muted mb-0">Payment Method:</p>
+            <p class="text-muted mb-0">Payment Status:</p>
           </div>
           <div class="flex-grow-1 ms-2">
-            <h6 class="mb-0">Debit Card</h6>
+            <h6 class="mb-0">Paid</h6>
           </div>
         </div>
         <div class="d-flex align-items-center mb-2">
           <div class="flex-shrink-0">
-            <p class="text-muted mb-0">Card Holder Name:</p>
+            <p class="text-muted mb-0">Date Paid:</p>
           </div>
           <div class="flex-grow-1 ms-2">
-            <h6 class="mb-0">Joseph Parker</h6>
-          </div>
-        </div>
-        <div class="d-flex align-items-center mb-2">
-          <div class="flex-shrink-0">
-            <p class="text-muted mb-0">Card Number:</p>
-          </div>
-          <div class="flex-grow-1 ms-2">
-            <h6 class="mb-0">xxxx xxxx xxxx 2456</h6>
+            <h6 class="mb-0">
+              18 Jul, 2022 <small class="text-muted">11:30 AM</small></h6>
           </div>
         </div>
         <div class="d-flex align-items-center">
@@ -290,7 +279,7 @@
             <p class="text-muted mb-0">Total Amount:</p>
           </div>
           <div class="flex-grow-1 ms-2">
-            <h6 class="mb-0">$415.96</h6>
+            <h6 class="mb-0">₱1,750.00</h6>
           </div>
         </div>
       </div>
