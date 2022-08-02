27be0e538b837cf64a6478d4f2d47dd824f47f3b
paultulod <paultulod@pm.me>
2022-08-02 23:12:55 +0800

    Update html main

--- application/views/partials/main.php
+++ application/views/partials/main.php
@@ -1,8 +1,2 @@
 <!doctype html>
-<html lang="en" data-layout="horizontal" data-topbar="light" data-sidebar='dark' data-sidebar-image="none" data-layout-position="fixed">
-<!-- If RestoAdmin -->
-<!-- <html lang="en" data-layout="vertical" data-layout-style="detached" data-sidebar="light" data-topbar="light" data-sidebar-image="none" data-sidebar-size="lg"> -->
-<!-- If Admin -->
-<!-- <html lang="en" data-layout="vertical" data-topbar="dark" data-sidebar='dark' data-sidebar-image="none" data-layout-position="fixed"> -->
-<!-- If Customer -->
-<!-- <html lang="en" data-layout="horizontal" data-topbar="light" data-sidebar='dark' data-sidebar-image="none" data-layout-position="fixed"> -->
+<html lang="en" data-layout="vertical" data-topbar="dark" data-sidebar='dark' data-sidebar-image="none" data-layout-position="fixed">
