db23e109c5122e05fca05253e56bf58efbec53f9
paultulod <paultulod@pm.me>
2022-08-02 23:26:41 +0800

    Add Customer sidebar and topbar partials - Create sidebar - Create topbar Co-authored-by: micahvllz <97878801+micahvllz@users.noreply.github.com> Co-authored-by: emmanperalta4 <101327195+emmanperalta4@users.noreply.github.com> Co-authored-by: drollive <61725450+drollive@users.noreply.github.com> Co-authored-by: AshleeJude <104343358+AshleeJude@users.noreply.github.com> Co-authored-by: Josayah32 <80877792+Josayah32@users.noreply.github.com> Co-authored-by: racquelulibas <92304694+racquelulibas@users.noreply.github.com>

---
+++ application/views/partials/customer/sidebar.php
@@ -0,0 +1,122 @@
+        <!-- ========== App Menu ========== -->
+        <div class="app-menu navbar-menu">
+            <!-- LOGO -->
+            <div class="navbar-brand-box">
+                <!-- Dark Logo-->
+                <a href="<?php echo base_url() ?>customer/explore" class="logo logo-dark">
+                    <span class="logo-sm">
+                        <img src="<?php echo base_url('public/assets') ?>/images/logo-sm.png" alt="" height="35">
+                    </span>
+                    <span class="logo-lg">
+                        <img src="<?php echo base_url('public/assets') ?>/images/logo-dark.png" alt="" height="45">
+                    </span>
+                </a>
+                <!-- Light Logo-->
+                <a href="<?php echo base_url() ?>customer/explore" class="logo logo-light">
+                    <span class="logo-sm">
+                        <img src="<?php echo base_url('public/assets') ?>/images/logo-sm.png" alt="" height="35">
+                    </span>
+                    <span class="logo-lg">
+                        <img src="<?php echo base_url('public/assets') ?>/images/logo-light.png" alt="" height="45">
+                    </span>
+                </a>
+                <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
+                    <i class="ri-record-circle-line"></i>
+                </button>
+            </div>
+
+            <div id="scrollbar">
+                <div class="container-fluid">
+
+                    <div id="two-column-menu">
+                    </div>
+                    <ul class="navbar-nav" id="navbar-nav">
+                        <li class="menu-title"><span data-key="t-menu">Categories</span></li>
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
+                                <i class="mdi mdi-speedometer"></i> <span data-key="t-dashboards">Restaurant Categories</span>
+                            </a>
+                            <div class="collapse menu-dropdown" id="sidebarDashboards">
+                                <ul class="nav nav-sm flex-column">
+                                    <li class="nav-item">
+                                        <a href="dashboard-analytics.html" class="nav-link" data-key="t-analytics"> Analytics </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="dashboard-crm.html" class="nav-link" data-key="t-crm"> CRM </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="index.html" class="nav-link" data-key="t-ecommerce"> Ecommerce </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="dashboard-crypto.html" class="nav-link" data-key="t-crypto"> Crypto </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="dashboard-projects.html" class="nav-link" data-key="t-projects"> Projects </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="dashboard-nft.html" class="nav-link" data-key="t-nft"> NFT <span class="badge badge-pill bg-danger" data-key="t-new">New</span></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="#sidebarLayouts" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
+                                <i class="mdi mdi-view-carousel-outline"></i> <span data-key="t-layouts">Dish Categories</span>
+                            </a>
+                            <div class="collapse menu-dropdown" id="sidebarLayouts">
+                                <ul class="nav nav-sm flex-column">
+                                    <li class="nav-item">
+                                        <a href="layouts-horizontal.html" class="nav-link" target="_blank" data-key="t-horizontal">Horizontal</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="layouts-detached.html" class="nav-link" target="_blank" data-key="t-detached">Detached</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="layouts-two-column.html" class="nav-link" target="_blank" data-key="t-two-column">Two Column</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="layouts-vertical-hovered.html" class="nav-link" target="_blank" data-key="t-hovered">Hovered</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-pages">Restaurants and Dishes</span></li>
+
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="<?php echo base_url() ?>customer/restaurants" role="button" aria-expanded="false" aria-controls="sidebarAuth">
+                                <i class="mdi mdi-account-circle-outline"></i> <span data-key="t-authentication">Restaurants</span>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="<?php echo base_url() ?>customer/products" role="button" aria-expanded="false" aria-controls="sidebarPages">
+                                <i class="mdi mdi-sticker-text-outline"></i> <span data-key="t-pages">Dishes</span>
+                            </a>
+                        </li>
+
+                        <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-components">Orders & Payments</span></li>
+
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="<?php echo base_url() ?>customer/orders" role="button" aria-expanded="false" aria-controls="sidebarUI">
+                                <i class="mdi mdi-cube-outline"></i> <span data-key="t-base-ui">Orders</span>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="<?php echo base_url() ?>customer/invoices" role="button" aria-expanded="false" aria-controls="sidebarAdvanceUI">
+                                <i class="mdi mdi-layers-triple-outline"></i> <span data-key="t-advance-ui">Payments</span>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+                <!-- Sidebar -->
+            </div>
+
+            <div class="sidebar-background"></div>
+        </div>
+        <!-- Left Sidebar End -->
+        <!-- Vertical Overlay-->
+        <div class="vertical-overlay"></div>
