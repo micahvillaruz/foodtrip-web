1915f6d9371ed65c918aef862dc646ed812c126e
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-08-06 14:51:50 +0800

    Connect RestoAdmin - View Orders to API

--- application/views/restoadmin/orders-management/components/modals/edit-order-modal.php
+++ application/views/restoadmin/orders-management/components/modals/edit-order-modal.php
@@ -18,33 +18,11 @@
             <label for="order_status" class="form-label">Status</label>
             <select class="form-select" id="order_status" name="order_status" data-choices data-choices-sorting-false data-choices-search-false>
               <option value="">Select Status</option>
-              <option value="1">Pending</option>
-              <option value="2">Cancelled</option>
-              <option value="3">In Process</option>
-              <option value="4">On the Way</option>
-              <option value="5">Delivered</option>
-              <option value="6">Rejected</option>
+              <option value="1">In Process</option>
+              <option value="2">On the Way</option>
+              <option value="3">Delivered</option>
+              <option value="4">Rejected</option>
             </select>
-          </div>
-
-          <div class="mb-3 process-date">
-            <label for="date_processed" class="form-label">Date Processed</label>
-            <input type="date" id="date_processed" name="date_processed" class="form-control" data-provider="flatpickr" data-date-format="d M, Y" data-enable-time required placeholder="Select Date" />
-          </div>
-
-          <div class="mb-3 cancel-date">
-            <label for="date_cancelled" class="form-label">Date Cancelled</label>
-            <input type="date" id="date_cancelled" name="date_cancelled" class="form-control" data-provider="flatpickr" data-date-format="d M, Y" data-enable-time required placeholder="Select Date" />
-          </div>
-
-          <div class="mb-3 release-date">
-            <label for="date_released" class="form-label">Date Released</label>
-            <input type="date" id="date_released" name="date_released" class="form-control" data-provider="flatpickr" data-date-format="d M, Y" data-enable-time required placeholder="Select Date" />
-          </div>
-
-          <div class="reject-date">
-            <label for="date_rejected" class="form-label">Date Rejected</label>
-            <input type="date" id="date_rejected" name="date_rejected" class="form-control" data-provider="flatpickr" data-date-format="d M, Y" data-enable-time required placeholder="Select Date" />
           </div>
 
           <div class="mt-3 courier-name">
@@ -68,11 +46,6 @@
           <div class="my-3 driver-phone">
             <label class="form-label" for="driver_phone">Driver Phone</label>
             <input type="number" class="form-control" id="driver_phone" name="driver_phone" placeholder="Enter Driver Phone" required>
-          </div>
-
-          <div class="date-received">
-            <label for="date_received" class="form-label">Date Received</label>
-            <input type="date" id="date_received" name="date_received" class="form-control" data-provider="flatpickr" data-date-format="d M, Y" data-enable-time required placeholder="Select date" />
           </div>
 
         </div>
