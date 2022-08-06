59402cd6450ac1f04151702c61d75ba0442ff189
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-08-07 00:20:48 +0800

    Connect Orders and OrderDetails to API - Partial

--- application/views/restoadmin/orders-management/pages/order-details.php
+++ application/views/restoadmin/orders-management/pages/order-details.php
@@ -3,9 +3,9 @@
     <div class="card">
       <div class="card-header">
         <div class="d-flex align-items-center">
-          <h5 class="card-title flex-grow-1 mb-0">Order #97464507680</h5>
+          <h5 class="card-title flex-grow-1 mb-0">Order <span id="order_no"></span></h5>
           <div class="flex-shrink-0">
-            <a href="apps-invoices-details.html" class="btn btn-success btn-sm"><i class="ri-download-2-fill align-middle me-1"></i> Invoice</a>
+            <a href="#!" class="btn btn-success btn-sm"><i class="ri-download-2-fill align-middle me-1"></i> Invoice</a>
           </div>
         </div>
       </div>
@@ -21,73 +21,21 @@
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>
-                  <div class="d-flex">
-                    <div class="flex-shrink-0 avatar-md bg-light rounded p-1">
-                      <img src="<?php echo base_url('public/assets') ?>/images/food/jollibee/img-27.png" alt="" class="img-fluid d-block">
-                    </div>
-                    <div class="flex-grow-1 ms-3">
-                      <h5 class="fs-15"><a href="#!" class="link-primary">8pc Chickenjoy w/ Jolly Spaghetti Pan</a></h5>
-                      <p class="text-muted mb-0">Category: <span class="fw-medium">Family Meals</span></p>
-                    </div>
-                  </div>
-                </td>
-                <td>₱776.00</td>
-                <td>1</td>
-                <td class="fw-medium text-end">
-                  ₱776.00
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div class="d-flex">
-                    <div class="flex-shrink-0 avatar-md bg-light rounded p-1">
-                      <img src="<?php echo base_url('public/assets') ?>/images/food/jollibee/img-15.png" alt="" class="img-fluid d-block">
-                    </div>
-                    <div class="flex-grow-1 ms-3">
-                      <h5 class="fs-15"><a href="#!" class="link-primary">Amazing Aloha Champ Jr.</a></h5>
-                      <p class="text-muted mb-0">Category: <span class="fw-medium">Burgers</span></p>
-                    </div>
-                  </div>
-                </td>
-                <td>₱109.00</td>
-                <td>5</td>
-                <td class="fw-medium text-end">
-                  ₱545.00
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div class="d-flex">
-                    <div class="flex-shrink-0 avatar-md bg-light rounded p-1">
-                      <img src="<?php echo base_url('public/assets') ?>/images/food/jollibee/img-28.png" alt="" class="img-fluid d-block">
-                    </div>
-                    <div class="flex-grow-1 ms-3">
-                      <h5 class="fs-15"><a href="#!" class="link-primary">1pc Burger Steak w/ Shanghai w/ Drink</a></h5>
-                      <p class="text-muted mb-0">Category: <span class="fw-medium">Burger Steak</span></p>
-                    </div>
-                  </div>
-                </td>
-                <td>₱143.00</td>
-                <td>3</td>
-                <td class="fw-medium text-end">
-                  ₱429.00
-                </td>
-              </tr>
+              <div id="dishes">
+              </div>
               <tr class="border-top border-top-dashed">
                 <td colspan="2">
                   <span class="badge rounded-pill bg-primary fs-12">Order Comment</span>
-                  <div class="bg-light ms-1 col-6 text-wrap">
-                    <p class="p-2">Please provide extra rice and extra gravy and if pwede po thigh and leg part po thank u</p>
+                  <div class="bg-light ms-1 col-10 text-wrap">
+                    <p id="comment" class="p-2"></p>
                   </div>
                 </td>
-                <td colspan="2" class="fw-medium p-0">
+                <td colspan="2" class="fw-medium p-0 align-bottom">
                   <table class="table table-borderless mb-0">
                     <tbody>
-                      <tr class="align-bottom">
+                      <tr>
                         <th scope="row"><span class="fs-15">Total (PHP) :</span></th>
-                        <th class="text-end"><span class="fs-15">₱1,750.00</span></th>
+                        <th class="text-end"><span id="total" class="fs-15"></span></th>
                       </tr>
                     </tbody>
                   </table>
@@ -121,7 +69,7 @@
                       </div>
                     </div>
                     <div class="flex-grow-1 ms-3">
-                      <h6 class="fs-15 mb-0 fw-semibold">Pending - <span class="fw-normal">Mon, 18 Jul 2022</span></h6>
+                      <h6 class="fs-15 mb-0 fw-semibold">Pending - <span id="date_created" class="fw-normal"></span></h6>
                     </div>
                   </div>
                 </a>
@@ -129,76 +77,15 @@
               <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                 <div class="accordion-body ms-2 ps-5 py-0">
                   <h6 class="mb-1">An order has been placed.</h6>
-                  <p class="text-muted">Mon, 18 Jul 2022 - 11:30AM</p>
+                  <p id="full_date_created" class="text-muted"></p>
                 </div>
               </div>
             </div>
-            <div class="accordion-item border-0">
-              <div class="accordion-header" id="headingTwo">
-                <a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                  <div class="d-flex align-items-center">
-                    <div class="flex-shrink-0 avatar-xs">
-                      <div class="avatar-title bg-success rounded-circle shadow">
-                        <i class="bx bx-dish"></i>
-                      </div>
-                    </div>
-                    <div class="flex-grow-1 ms-3">
-                      <h6 class="fs-15 mb-0 fw-semibold">In Process - <span class="fw-normal">Mon, 18 Jul 2022</span></h6>
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                <div class="accordion-body ms-2 ps-5 py-0">
-                  <h6 class="mb-1">Seller has proccessed your order.</h6>
-                  <p class="text-muted">Mon, 18 Jul 2022 - 12:00PM</p>
-                </div>
-              </div>
+            <div id="in_process" class="accordion-item border-0">
             </div>
-            <div class="accordion-item border-0">
-              <div class="accordion-header" id="headingThree">
-                <a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                  <div class="d-flex align-items-center">
-                    <div class="flex-shrink-0 avatar-xs">
-                      <div class="avatar-title bg-success rounded-circle shadow">
-                        <i class="ri-truck-line"></i>
-                      </div>
-                    </div>
-                    <div class="flex-grow-1 ms-3">
-                      <h6 class="fs-15 mb-1 fw-semibold">On the Way - <span class="fw-normal">Mon, 18 Jul 2022</span></h6>
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div id="collapseThree" class="accordion-collapse collapse show" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                <div class="accordion-body ms-2 ps-5 pt-0">
-                  <h6 class="fs-14">LalaMove - 26325016208</h6>
-                  <h6 class="mb-1">The order has been picked up by the courier partner.</h6>
-                  <p class="text-muted mb-0">Mon, 18 Jul 2022 - 12:15PM</p>
-                </div>
-              </div>
+            <div id="on_the_way" class="accordion-item border-0">
             </div>
-            <div class="accordion-item border-0">
-              <div class="accordion-header" id="headingFour">
-                <a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                  <div class="d-flex align-items-center">
-                    <div class="flex-shrink-0 avatar-xs">
-                      <div class="avatar-title bg-success rounded-circle shadow">
-                        <i class="mdi mdi-package-variant"></i>
-                      </div>
-                    </div>
-                    <div class="flex-grow-1 ms-3">
-                      <h6 class="fs-15 mb-1 fw-semibold">Delivered - <span class="fw-normal">Mon, 18 Jul 2022</span></h6>
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div id="collapseFour" class="accordion-collapse collapse show" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-                <div class="accordion-body ms-2 ps-5 pt-0">
-                  <h6 class="mb-1">The order has been delivered.</h6>
-                  <p class="text-muted mb-0">Mon, 18 Jul 2022 - 12:29PM</p>
-                </div>
-              </div>
+            <div id="delivered" class="accordion-item border-0">
             </div>
           </div>
           <!--end accordion-->
@@ -209,24 +96,7 @@
   </div>
   <!--end col-->
   <div class="col-xl-3">
-    <div class="card">
-      <div class="card-header">
-        <div class="d-flex">
-          <h5 class="card-title flex-grow-1 mb-0"><i class="mdi mdi-truck-fast-outline align-middle me-1 text-muted"></i> Delivery Details</h5>
-          <div class="flex-shrink-0">
-            <a href="javascript:void(0);" class="badge badge-soft-primary fs-11">Track Order</a>
-          </div>
-        </div>
-      </div>
-      <div class="card-body">
-        <div class="text-center">
-          <lord-icon src="https://cdn.lordicon.com/uetqnvvg.json" trigger="loop" colors="primary:#4b38b3,secondary:#0ab39c" style="width:80px;height:80px"></lord-icon>
-          <h5 class="fs-16 mt-2">LalaMove</h5>
-          <p class="text-muted mb-0">Tracking No: 26325016208</p>
-          <p class="text-muted mb-0">Driver Name : Adonis Saide Figueroa</p>
-          <p class="text-muted mb-0">Driver Phone : 09918291232</p>
-        </div>
-      </div>
+    <div id="delivery-details" class="card">
     </div>
     <!--end card-->
 
@@ -244,13 +114,13 @@
           <li>
             <div class="d-flex align-items-center">
               <div class="flex-grow-1 ms-3">
-                <h6 class="fs-14 mb-1">Avrielle Haven Fernandez Juarez</h6>
+                <h6 id="customer_name" class="fs-14 mb-1"></h6>
                 <p class="text-muted mb-0">Customer</p>
               </div>
             </div>
           </li>
-          <li><i class="ri-mail-line me-2 align-middle text-muted fs-16"></i>avijuarez@gmail.com</li>
-          <li><i class="ri-phone-line me-2 align-middle text-muted fs-16"></i>09872537193</li>
+          <li><i class="ri-mail-line me-2 align-middle text-muted fs-16"></i><span id="email"></span></li>
+          <li><i class="ri-phone-line me-2 align-middle text-muted fs-16"></i><span id="phone"></span></li>
         </ul>
       </div>
     </div>
@@ -261,11 +131,11 @@
       </div>
       <div class="card-body">
         <ul class="list-unstyled vstack gap-2 fs-13 mb-0">
-          <li class="fw-medium fs-14">Avrielle Haven Fernandez Juarez</li>
-          <li>09872537193</li>
-          <li>1071 Del Monte Avenue</li>
-          <li>Barangay Paltok, Quezon City</li>
-          <li>Metro Manila, 1105</li>
+          <li class="fw-medium fs-14"><span id="address_owner"></span></li>
+          <li><span id="address_owner_phone"></span></li>
+          <li><span id="address_first_line"></span></li>
+          <li><span id="address_second_line"></span></li>
+          <li><span id="address_third_line"></span></li>
         </ul>
       </div>
     </div>
@@ -316,3 +186,6 @@
   <!--end col-->
 </div>
 <!--end row-->
+
+<!-- orders ajax js -->
+<script src="<?php echo base_url('public/assets') ?>/js/ajax/restoadmin/order-details.ajax.js"></script>
---
+++ public/assets/js/ajax/restoadmin/order-details.ajax.js
@@ -0,0 +1,314 @@
+$(function () {
+	viewOrderDetails();
+});
+
+// View Order Details
+viewOrderDetails = (order_id) => {
+	$.ajaxSetup({
+		headers: {
+			Accept: "application/json",
+			Authorization: "Bearer " + token,
+			ContentType: "application/x-www-form-urlencoded",
+		},
+	});
+
+	$.ajax({
+		type: "GET",
+		cache: false,
+		url: apiURL + `resto-admin/order/${order_id}`,
+		dataType: "json",
+		success: (result) => {
+			const data = result.data;
+
+			let orderNo = data.order_no;
+			$("#order_no").html(orderNo);
+
+			let orderDishes = "";
+			data.order_dishes.forEach((dish) => {
+				orderDishes += `
+						<tr>
+							<td>
+								<div class="d-flex">
+									<div class="flex-shrink-0 avatar-md bg-light rounded p-1">
+										<img src="${dish.dish_img}" alt="" class="img-fluid d-block">
+									</div>
+									<div class="flex-grow-1 ms-3">
+										<h5 class="fs-15">
+											<a href="#!" class="link-primary">${dish.dish_name}</a>
+										</h5>
+										<p class="text-muted mb-0">Category: 
+											<span class="fw-medium">${dish.dish_category.dishcatg_name}</span>
+										</p>
+									</div>
+								</div>
+							</td>
+							<td>
+								<span>₱ ${dish.dish_price}</span>
+							</td>
+							<td>
+								<span>${dish.OrderDetails.quantity}</span></td>
+							<td class="text-end">
+								<span class="fw-medium"> ₱
+								${dish.OrderDetails.subtotal}
+								</span>
+							</td>
+					</tr>
+				`;
+			});
+			$("dishes").html(orderDishes);
+
+			$("comment").html(data.order_comment);
+
+			let orderTotal = data.order_total;
+			orderTotal = `₱ ${data.order_total}`;
+			$("total").html(orderTotal);
+
+			const pending_date = data.date_created;
+			let pendingDate = `
+				${moment(pending_date).format("ddd")}, ${moment(pending_date).format(
+				"D MMM YYYY"
+			)}
+			`;
+			$("date_created").html(pendingDate);
+
+			let pendingDateFull = `
+				${moment(pending_date).format("ddd")}, ${moment(pending_date).format(
+				"D MMM YYYY"
+			)} - ${moment(pending_date).format("hh:mm A")}
+			`;
+			$("full_date_created").html(pendingDateFull);
+
+			let inProcess = "";
+			if (data.date_processed !== null) {
+				inProcess += `
+					<div class="accordion-header" id="headingTwo">
+						<a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+							<div class="d-flex align-items-center">
+								<div class="flex-shrink-0 avatar-xs">
+									<div class="avatar-title bg-success rounded-circle shadow">
+										<i class="bx bx-dish"></i>
+									</div>
+								</div>
+								<div class="flex-grow-1 ms-3">
+									<h6 class="fs-15 mb-0 fw-semibold">In Process - 
+										<span class="fw-normal">
+											${moment(data.date_processed).format("ddd")}, 
+											${moment(data.date_processed).format("D MMM YYYY")}
+										</span>
+									</h6>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+						<div class="accordion-body ms-2 ps-5 py-0">
+							<h6 class="mb-1">Seller has proccessed your order.</h6>
+							<p class="text-muted">
+								${moment(data.date_processed).format("ddd")},
+								${moment(data.date_processed).format("D MMM YYYY")} - 
+								${moment(data.date_processed).format("hh:mm A")}
+							</p>
+						</div>
+					</div>
+				`;
+				$("in_process").html(inProcess);
+			} else if (data.date_processed === null) {
+				inProcess += `
+					<div class="accordion-header" id="headingTwo">
+							<a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseTwo" aria-expanded="false">
+								<div class="d-flex align-items-center">
+									<div class="flex-shrink-0 avatar-xs">
+										<div class="avatar-title bg-light text-success rounded-circle shadow">
+											<i class="bx bx-dish"></i>
+										</div>
+									</div>
+									<div class="flex-grow-1 ms-3">
+										<h6 class="fs-15 mb-0 fw-semibold">In Process</h6>
+									</div>
+								</div>
+							</a>
+						</div>
+				`;
+				$("in_process").html(inProcess);
+			}
+
+			let onTheWay = "";
+			if (data.date_released !== null) {
+				onTheWay += `
+					<div class="accordion-header" id="headingThree">
+						<a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+							<div class="d-flex align-items-center">
+								<div class="flex-shrink-0 avatar-xs">
+									<div class="avatar-title bg-success rounded-circle shadow">
+										<i class="ri-truck-line"></i>
+									</div>
+								</div>
+								<div class="flex-grow-1 ms-3">
+									<h6 class="fs-15 mb-1 fw-semibold">On the Way - 
+										<span class="fw-normal">
+											${moment(data.date_released).format("ddd")}, 
+											${moment(data.date_released).format("D MMM YYYY")}
+										</span>
+									</h6>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div id="collapseThree" class="accordion-collapse collapse show" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+						<div class="accordion-body ms-2 ps-5 pt-0">
+							<h6 class="fs-14">LalaMove - 26325016208</h6>
+							<h6 class="mb-1">The order has been picked up by the courier partner.</h6>
+							<p class="text-muted mb-0">
+								${moment(data.date_released).format("ddd")},
+								${moment(data.date_released).format("D MMM YYYY")} - 
+								${moment(data.date_released).format("hh:mm A")}
+							</p>
+						</div>
+					</div>
+				`;
+				$("on_the_way").html(onTheWay);
+			} else if (data.date_released === null) {
+				onTheWay += `
+					<div class="accordion-header" id="headingThree">
+						<a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseThree" aria-expanded="false">
+							<div class="d-flex align-items-center">
+								<div class="flex-shrink-0 avatar-xs">
+									<div class="avatar-title bg-light text-success rounded-circle shadow">
+										<i class="ri-truck-line"></i>
+									</div>
+								</div>
+								<div class="flex-grow-1 ms-3">
+									<h6 class="fs-15 mb-0 fw-semibold">On the Way</h6>
+								</div>
+							</div>
+						</a>
+					</div>
+				`;
+				$("on_the_way").html(onTheWay);
+			}
+
+			let Delivered = "";
+			if (data.order_status === "Delivered") {
+				let date_delivered = new Date();
+				Delivered += `
+					<div class="accordion-header" id="headingFour">
+						<a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+							<div class="d-flex align-items-center">
+								<div class="flex-shrink-0 avatar-xs">
+									<div class="avatar-title bg-success rounded-circle shadow">
+										<i class="mdi mdi-package-variant"></i>
+									</div>
+								</div>
+								<div class="flex-grow-1 ms-3">
+									<h6 class="fs-15 mb-1 fw-semibold">Delivered - 
+										<span class="fw-normal">
+											${moment(date_delivered).format("ddd")}, 
+											${moment(date_delivered).format("D MMM YYYY")}
+										</span>
+									</h6>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div id="collapseFour" class="accordion-collapse collapse show" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+						<div class="accordion-body ms-2 ps-5 pt-0">
+							<h6 class="mb-1">The order has been delivered.</h6>
+							<p class="text-muted mb-0">
+								${moment(data.date_delivered).format("ddd")},
+								${moment(data.date_delivered).format("D MMM YYYY")} - 
+								${moment(data.date_delivered).format("hh:mm A")}
+							</p>
+						</div>
+					</div>
+				`;
+				$("delivered").html(Delivered);
+			} else if (data.order_status !== "Delivered") {
+				Delivered += `
+					<div class="accordion-header" id="headingFour">
+						<a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseFour" aria-expanded="false">
+							<div class="d-flex align-items-center">
+								<div class="flex-shrink-0 avatar-xs">
+									<div class="avatar-title bg-light text-success rounded-circle shadow">
+										<i class="mdi mdi-package-variant"></i>
+									</div>
+								</div>
+								<div class="flex-grow-1 ms-3">
+									<h6 class="fs-15 mb-0 fw-semibold">Delivered</h6>
+								</div>
+							</div>
+						</a>
+					</div>
+				`;
+				$("delivered").html(Delivered);
+			}
+
+			let deliveryDetails = "";
+			if (data.delivery_details !== null) {
+				deliveryDetails += `
+				<div class="card-header">
+					<div class="d-flex">
+						<h5 class="card-title flex-grow-1 mb-0"><i class="mdi mdi-truck-fast-outline align-middle me-1 text-muted"></i> Delivery Details</h5>
+						<div class="flex-shrink-0">
+							<a href="javascript:void(0);" class="badge badge-soft-primary fs-11">Track Order</a>
+						</div>
+					</div>
+				</div>
+				<div class="card-body">
+					<div class="text-center">
+						<lord-icon src="https://cdn.lordicon.com/uetqnvvg.json" trigger="loop" colors="primary:#4b38b3,secondary:#0ab39c" style="width:80px;height:80px"></lord-icon>
+						<h5 class="fs-16 mt-2">LalaMove</h5>
+						<p class="text-muted mb-0">Tracking No: 26325016208</p>
+						<p class="text-muted mb-0">Driver Name : Adonis Saide Figueroa</p>
+						<p class="text-muted mb-0">Driver Phone : 09918291232</p>
+					</div>
+				</div>
+				`;
+			} else if (data.deliveryDetails === null) {
+				deliveryDetails += `
+				<div class="card-header">
+					<div class="d-flex">
+						<h5 class="card-title flex-grow-1 mb-0"><i class="mdi mdi-truck-fast-outline align-middle me-1 text-muted"></i> Delivery Details</h5>
+						<div class="flex-shrink-0">
+							<a href="javascript:void(0);" class="badge badge-soft-primary fs-11">Track Order</a>
+						</div>
+					</div>
+				</div>
+				<div class="card-body">
+					<div class="text-center">
+						<i class="h1 mdi mdi-close-circle-outline text-danger"></i>
+						<h5 class="fs-16 mt-2">Order has not yet been shipped.</h5>
+						<p class="text-muted mb-0">No delivery information available</p>
+					</div>
+				</div>
+				`;
+			}
+
+			const fullName = `${data.created.first_name}  ${
+				data.created.middle_name === null ? "" : data.created.middle_name
+			}  ${data.created.last_name}`;
+
+			$("customer_name").html(fullName);
+			$("email").html(data.created.email_address);
+			$("phone").html(data.created.phone_number);
+
+			$("address_owner").html(data.address.full_name);
+			$("address_owner_phone").html(data.address.phone_number);
+
+			let addressFirstLine = `${data.address.address_1} ${
+				data.address.address_2 === null ? "" : data.address.address_2
+			}`;
+
+			$("address_first_line").html(addressFirstLine);
+
+			let addressSecondLine = `${data.address.barangay} ${data.address.city}`;
+
+			$("address_second_line").html(addressSecondLine);
+
+			let addressThirdLine = `${data.address.province} ${data.address.region} ${data.address.zip_code}`;
+
+			$("address_third_line").html(addressThirdLine);
+		},
+	}).fail(() =>
+		console.error("There was an error in getting the order details")
+	);
+};
