a42442df47555e75292f4299fbb3dd9ab6bf049c
paultulod <paultulod@pm.me>
2022-08-09 03:44:54 +0800

    Connect order-details page - Update order-details php - Add order details ajax

--- application/views/customer/orders/pages/order-details.php
+++ application/views/customer/orders/pages/order-details.php
@@ -3,9 +3,9 @@
         <div class="card">
             <div class="card-header">
                 <div class="d-flex align-items-center">
-                    <h5 class="card-title flex-grow-1 mb-0">Order #97464507680</h5>
+                    <h5 id="order_no" class="card-title flex-grow-1 mb-0 fw-medium link-primary"></h5>
                     <div class="flex-shrink-0">
-                        <a href="apps-invoices-details.html" class="btn btn-success btn-sm"><i class="ri-download-2-fill align-middle me-1"></i> Invoice</a>
+                        <a href="<?php echo base_url() ?>customer/invoices/invoice-details" class="btn btn-success btn-sm"><i class="ri-download-2-fill align-middle me-1"></i> Invoice</a>
                     </div>
                 </div>
             </div>
@@ -20,77 +20,27 @@
                                 <th scope="col" class="text-end">Sub Total</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <div class="d-flex">
-                                        <div class="flex-shrink-0 avatar-md bg-light rounded p-1">
-                                            <img src="<?php echo base_url('public/assets') ?>/images/food/jollibee/img-27.png" alt="" class="img-fluid d-block">
-                                        </div>
-                                        <div class="flex-grow-1 ms-3">
-                                            <h5 class="fs-15"><a href="#!" class="link-primary">8pc Chickenjoy w/ Jolly Spaghetti Pan</a></h5>
-                                            <p class="text-muted mb-0">Category: <span class="fw-medium">Family Meals</span></p>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>₱776.00</td>
-                                <td>1</td>
-                                <td class="fw-medium text-end">
-                                    ₱776.00
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="d-flex">
-                                        <div class="flex-shrink-0 avatar-md bg-light rounded p-1">
-                                            <img src="<?php echo base_url('public/assets') ?>/images/food/jollibee/img-15.png" alt="" class="img-fluid d-block">
-                                        </div>
-                                        <div class="flex-grow-1 ms-3">
-                                            <h5 class="fs-15"><a href="#!" class="link-primary">Amazing Aloha Champ Jr.</a></h5>
-                                            <p class="text-muted mb-0">Category: <span class="fw-medium">Burgers</span></p>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>₱109.00</td>
-                                <td>5</td>
-                                <td class="fw-medium text-end">
-                                    ₱545.00
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="d-flex">
-                                        <div class="flex-shrink-0 avatar-md bg-light rounded p-1">
-                                            <img src="<?php echo base_url('public/assets') ?>/images/food/jollibee/img-28.png" alt="" class="img-fluid d-block">
-                                        </div>
-                                        <div class="flex-grow-1 ms-3">
-                                            <h5 class="fs-15"><a href="#!" class="link-primary">1pc Burger Steak w/ Shanghai w/ Drink</a></h5>
-                                            <p class="text-muted mb-0">Category: <span class="fw-medium">Burger Steak</span></p>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>₱143.00</td>
-                                <td>3</td>
-                                <td class="fw-medium text-end">
-                                    ₱429.00
-                                </td>
-                            </tr>
+                        <tbody id="dishes">
                             <tr class="border-top border-top-dashed">
                                 <td colspan="2">
-                                    <span class="badge rounded-pill bg-primary fs-12">Order Comment</span>
-                                    <div class="bg-light ms-1 col-6 text-wrap">
-                                        <p class="p-2">Please provide extra rice and extra gravy and if pwede po thigh and leg part po thank u</p>
-                                    </div>
                                 </td>
-                                <td colspan="2" class="fw-medium p-0">
+                                <td colspan="2" class="fw-medium p-0 align-center">
                                     <table class="table table-borderless mb-0">
                                         <tbody>
-                                            <tr class="align-bottom">
+                                            <tr>
                                                 <th scope="row"><span class="fs-15">Total (PHP) :</span></th>
-                                                <th class="text-end"><span class="fs-15">₱1,750.00</span></th>
+                                                <th class="text-end"><span id="total" class="fs-15"></span></th>
                                             </tr>
                                         </tbody>
                                     </table>
+                                </td>
+                            </tr>
+                            <tr class="border-top border-top-dashed">
+                                <td colspan="4">
+                                    <span class="badge rounded-pill bg-primary fs-12">Order Comment</span>
+                                    <div class="bg-light ms-1 col-10 text-wrap">
+                                        <p id="comment" class="p-2"></p>
+                                    </div>
                                 </td>
                             </tr>
                         </tbody>
@@ -118,7 +68,7 @@
                                             </div>
                                         </div>
                                         <div class="flex-grow-1 ms-3">
-                                            <h6 class="fs-15 mb-0 fw-semibold">Pending - <span class="fw-normal">Mon, 18 Jul 2022</span></h6>
+                                            <h6 class="fs-15 mb-0 fw-semibold">Pending - <span id="date_created" class="fw-normal"></span></h6>
                                         </div>
                                     </div>
                                 </a>
@@ -126,76 +76,15 @@
                             <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                                 <div class="accordion-body ms-2 ps-5 py-0">
                                     <h6 class="mb-1">An order has been placed.</h6>
-                                    <p class="text-muted">Mon, 18 Jul 2022 - 11:30AM</p>
+                                    <p id="full_date_created" class="text-muted"></p>
                                 </div>
                             </div>
                         </div>
-                        <div class="accordion-item border-0">
-                            <div class="accordion-header" id="headingTwo">
-                                <a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                                    <div class="d-flex align-items-center">
-                                        <div class="flex-shrink-0 avatar-xs">
-                                            <div class="avatar-title bg-success rounded-circle shadow">
-                                                <i class="bx bx-dish"></i>
-                                            </div>
-                                        </div>
-                                        <div class="flex-grow-1 ms-3">
-                                            <h6 class="fs-15 mb-0 fw-semibold">In Process - <span class="fw-normal">Mon, 18 Jul 2022</span></h6>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                                <div class="accordion-body ms-2 ps-5 py-0">
-                                    <h6 class="mb-1">Seller has proccessed your order.</h6>
-                                    <p class="text-muted">Mon, 18 Jul 2022 - 12:00PM</p>
-                                </div>
-                            </div>
+                        <div id="in_process" class="accordion-item border-0">
                         </div>
-                        <div class="accordion-item border-0">
-                            <div class="accordion-header" id="headingThree">
-                                <a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                    <div class="d-flex align-items-center">
-                                        <div class="flex-shrink-0 avatar-xs">
-                                            <div class="avatar-title bg-success rounded-circle shadow">
-                                                <i class="ri-truck-line"></i>
-                                            </div>
-                                        </div>
-                                        <div class="flex-grow-1 ms-3">
-                                            <h6 class="fs-15 mb-1 fw-semibold">On the Way - <span class="fw-normal">Mon, 18 Jul 2022</span></h6>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div id="collapseThree" class="accordion-collapse collapse show" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                                <div class="accordion-body ms-2 ps-5 pt-0">
-                                    <h6 class="fs-14">LalaMove - 26325016208</h6>
-                                    <h6 class="mb-1">The order has been picked up by the courier partner.</h6>
-                                    <p class="text-muted mb-0">Mon, 18 Jul 2022 - 12:15PM</p>
-                                </div>
-                            </div>
+                        <div id="on_the_way" class="accordion-item border-0">
                         </div>
-                        <div class="accordion-item border-0">
-                            <div class="accordion-header" id="headingFour">
-                                <a class="accordion-button p-2 shadow-none" data-bs-toggle="collapse" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                    <div class="d-flex align-items-center">
-                                        <div class="flex-shrink-0 avatar-xs">
-                                            <div class="avatar-title bg-success rounded-circle shadow">
-                                                <i class="mdi mdi-package-variant"></i>
-                                            </div>
-                                        </div>
-                                        <div class="flex-grow-1 ms-3">
-                                            <h6 class="fs-15 mb-1 fw-semibold">Delivered - <span class="fw-normal">Mon, 18 Jul 2022</span></h6>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div id="collapseFour" class="accordion-collapse collapse show" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-                                <div class="accordion-body ms-2 ps-5 pt-0">
-                                    <h6 class="mb-1">The order has been delivered.</h6>
-                                    <p class="text-muted mb-0">Mon, 18 Jul 2022 - 12:29PM</p>
-                                </div>
-                            </div>
+                        <div id="delivered" class="accordion-item border-0">
                         </div>
                     </div>
                     <!--end accordion-->
@@ -206,7 +95,7 @@
     </div>
     <!--end col-->
     <div class="col-xl-3">
-        <div class="card">
+        <!-- <div class="card">
             <div class="card-header">
                 <div class="d-flex">
                     <h5 class="card-title flex-grow-1 mb-0"><i class="mdi mdi-truck-fast-outline align-middle me-1 text-muted"></i> Delivery Details</h5>
@@ -224,34 +113,11 @@
                     <p class="text-muted mb-0">Driver Phone : 09918291232</p>
                 </div>
             </div>
+        </div> -->
+        <div id="delivery-details" class="card">
         </div>
         <!--end card-->
-        <div class="card">
-            <div class="card-header">
-                <div class="d-flex">
-                    <h5 class="card-title flex-grow-1 mb-0">Restaurant Details</h5>
-                    <div class="flex-shrink-0">
-                        <a href="javascript:void(0);" class="link-secondary">View Restaurant</a>
-                    </div>
-                </div>
-            </div>
-            <div class="card-body">
-                <ul class="list-unstyled mb-0 vstack gap-3">
-                    <li>
-                        <div class="d-flex align-items-center">
-                            <div class="flex-shrink-0">
-                                <img src="<?php echo base_url('public/assets') ?>/images/restaurants/jollibee.jpg" alt="" class="avatar-sm rounded shadow">
-                            </div>
-                            <div class="flex-grow-1 ms-3">
-                                <h6 class="fs-14 mb-1">Jollibee</h6>
-                                <p class="text-muted mb-0">Fast Food</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li><i class="ri-mail-line me-2 align-middle text-muted fs-16"></i>feedback@jollibee.com.ph.</li>
-                    <li><i class="ri-phone-line me-2 align-middle text-muted fs-16"></i>(02) 8-7000</li>
-                </ul>
-            </div>
+        <div class="card" id="restaurantDetails">
         </div>
         <!--end card-->
         <div class="card">
@@ -260,11 +126,11 @@
             </div>
             <div class="card-body">
                 <ul class="list-unstyled vstack gap-2 fs-13 mb-0">
-                    <li class="fw-medium fs-14">Avrielle Haven Fernandez Juarez</li>
-                    <li>09872537193</li>
-                    <li>1071 Del Monte Avenue</li>
-                    <li>Barangay Paltok, Quezon City</li>
-                    <li>Metro Manila, 1105</li>
+                    <li class="fw-medium fs-14"><span id="address_owner"></span></li>
+                    <li><span id="address_owner_phone"></span></li>
+                    <li><span id="address_first_line"></span></li>
+                    <li><span id="address_second_line"></span></li>
+                    <li><span id="address_third_line"></span></li>
                 </ul>
             </div>
         </div>
@@ -280,7 +146,7 @@
                         <p class="text-muted mb-0">Payment No:</p>
                     </div>
                     <div class="flex-grow-1 ms-2">
-                        <h6 class="mb-0">60524012573</h6>
+                        <h6 id="payment_no" class="mb-0"></h6>
                     </div>
                 </div>
                 <div class="d-flex align-items-center mb-2">
@@ -288,7 +154,7 @@
                         <p class="text-muted mb-0">Payment Status:</p>
                     </div>
                     <div class="flex-grow-1 ms-2">
-                        <h6 class="mb-0">Paid</h6>
+                        <h6 id="payment_status" class="mb-0"></h6>
                     </div>
                 </div>
                 <div class="d-flex align-items-center mb-2">
@@ -296,15 +162,15 @@
                         <p class="text-muted mb-0">Date Paid:</p>
                     </div>
                     <div class="flex-grow-1 ms-2">
-                        <h6 class="mb-0">18 Jul,2022 11:30 AM</h6>
+                        <h6 id="payment_date" class="mb-0"></h6>
                     </div>
                 </div>
-                <div class="d-flex align-items-center mb-2">
+                <div class="d-flex align-items-center">
                     <div class="flex-shrink-0">
                         <p class="text-muted mb-0">Total Amount:</p>
                     </div>
                     <div class="flex-grow-1 ms-2">
-                        <h6 class="mb-0">₱1,750.00</h6>
+                        <h6 id="payment_total" class="mb-0"></h6>
                     </div>
                 </div>
             </div>
@@ -314,3 +180,4 @@
     <!--end col-->
 </div>
 <!--end row-->
+<script src="<?php echo base_url('public/assets') ?>/js/ajax/customer/order_details.ajax.js"></script>
